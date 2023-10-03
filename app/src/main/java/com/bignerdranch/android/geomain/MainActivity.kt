b0a993cc5ac7b9ac45247aed025089ddbedd5de5
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-10-03 12:29:21 +0700

    Передача верного ответа в CheatActivity

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -44,7 +44,8 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
         cheatButton.setOnClickListener {
-            val intent = Intent(this, CheatActivity::class.java)
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
             startActivity(intent)
         }
         updateQuestion()
